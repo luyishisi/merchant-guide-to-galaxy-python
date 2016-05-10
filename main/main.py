d79f20ef7ce7413547a7b72244368ce5e036630e
luyishisi <543429245@qq.com>
2016-05-10 16:31:15 +0800

    增加try,解决bug中input_3.txt.中的错误输入时应有的错误申明输出

--- main/main.py
+++ main/main.py
@@ -2,9 +2,12 @@
 from str_Resolve import *
 import fileinput
 
-for line in fileinput.input("../test-data/input_1.txt"):
+for line in fileinput.input("../test-data/input_3.txt"):
     str_line=line[:-1]
     #print str_line
-    str_return = str_Resolve(str_line)
-    if str_return:
-        print str_return
+    try:
+        str_return = str_Resolve(str_line)
+        if str_return:
+            print str_return
+    except:
+        print "I have no idea what you are talking about"
